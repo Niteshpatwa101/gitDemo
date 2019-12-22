55acf6431129ad477ffa8c43b0e27bcafbf9072e
Nitesh Patwa <patwanitesh1997@gmail.com>
2019-12-22 10:15:11 +0530

    This is my first commit
    Done using Eclipse.

---
+++ gitDemo/src/com/nitesh/demo/Calculator.java
@@ -0,0 +1,10 @@
+package com.nitesh.demo;
+
+public class Calculator {
+	public static void main(String[] args) {
+		int num1, num2;
+		num1 = 5;
+		num2 = 7;
+		System.out.println(num1 + " + " + num2 + " = "+(num1+num2));
+	}
+}
